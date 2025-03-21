b272cf84c82e9269b2a31b8637a1286ae9601ed2
leider diaz <leidermartinez2012@gmail.com>
2025-03-21 12:47:16 -0500

    first commit

---
+++ consumer.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const amqp = require('amqplib')
+const queue = 'cola2'
+
+function intensiveOperation() {
+    let i = 1e3
+    while (i--) {}
+}
+
+async function subscriber() {
+    const connection = await amqp.connect('amqp://guest:guest@10.6.101.93')
+    const channel = await connection.createChannel()
+
+    await channel.assertQueue(queue)
+
+    channel.consume(queue, (message) => {
+        
+
+        intensiveOperation()
+
+        console.log(`Mensaje recibido desde "${queue}"`)
+        console.log(message.content.toString())
+
+        channel.ack(message)
+    })
+}
+
+subscriber().catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
